262317583add3935098b5add35f5b06f9896ca6d
niceseee <niceseee@protonmail.com>
2020-12-02 16:32:25 +0800

    feat: add SliverPreferredSizeHeaderDelegate

---
+++ lib/widget/sliver_preferred_size_header_delegate.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SliverPreferredSizeHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final PreferredSizeWidget child;
+  final Color backgroundColor;
+  final double elevation;
+
+  const SliverPreferredSizeHeaderDelegate({this.child, this.backgroundColor, this.elevation = 0});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    Widget result = child;
+    if (backgroundColor != null) {
+      result = Container(
+        color: backgroundColor,
+        child: child,
+      );
+    }
+    if (elevation != null) {
+      result = Material(
+        elevation: elevation,
+        child: result,
+      );
+    }
+    return result;
+  }
+
+  @override
+  double get maxExtent => child.preferredSize.height;
+
+  @override
+  double get minExtent => child.preferredSize.height;
+
+  @override
+  bool shouldRebuild(covariant SliverPreferredSizeHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
